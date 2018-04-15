a6b2287aa89923dd77de0d5ad3fa8fbb9c1190a7
Vova <wizardsbureau@protonmail.com>
2018-04-15 19:57:13 +0300

    Added simple input&output model as alternative for server model

--- src/main/java/opendota/Main.java
+++ src/main/java/opendota/Main.java
@@ -1,20 +1,57 @@
 package opendota;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
     
 public class Main {
+    public static void main(String[] args) {
+        String INPUT_FILE = "C:\\Users\\Vova\\workspace\\parser_env\\3549145408_1618162017.dem";
+        String OUTPUT_FILE = "C:\\Users\\Vova\\workspace\\parser_env\\res.jsonl";
 
-    public static void main(String[] args) throws Exception {
-        HttpServer server = HttpServer.create(new InetSocketAddress(Integer.valueOf(args.length > 0 ? args[0] : "5600")), 0);
-        server.createContext("/", new MyHandler());
-        server.setExecutor(java.util.concurrent.Executors.newFixedThreadPool(4));
-        server.start();
+        simplifiedInOut(INPUT_FILE, OUTPUT_FILE);
+        // alternative config
+        // startServer(args);
+    }
+
+    public static void simplifiedInOut(String inputFile, String outputFile) {
+        // Input file from console
+        BufferedInputStream is = null;
+        BufferedOutputStream os = null;
+        try {
+            is = new BufferedInputStream(new FileInputStream(inputFile));
+            os = new BufferedOutputStream(new FileOutputStream(outputFile));
+            new Parse(is, os);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(is != null)
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            if(os != null)
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+        }
+    }
+
+    public static void startServer(String[] args) {
+        // POST to (local) server
+        try {
+            HttpServer server = HttpServer.create(new InetSocketAddress(Integer.valueOf(args.length > 0 ? args[0] : "5600")), 0);
+            server.createContext("/", new MyHandler());
+            server.setExecutor(java.util.concurrent.Executors.newFixedThreadPool(4));
+            server.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     
     static class MyHandler implements HttpHandler {
